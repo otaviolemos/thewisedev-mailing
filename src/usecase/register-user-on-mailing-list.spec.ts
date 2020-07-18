b0a4659d9b7fa49b0ccebce4a10bc2ff0c0ea7be
Otavio Lemos <otaviolemos@gmail.com>
2020-07-18 20:59:38 -0300

    test: register user on mailing list

--- src/usecase/register-user-on-mailing-list.spec.ts
+++ src/usecase/register-user-on-mailing-list.spec.ts
@@ -1,5 +1,5 @@
 import { User } from '../entities/user'
-import { UserRepository } from '../usecases/port/user-repository'
+import { UserRepository } from './port/user-repository'
 import { InMemoryUserRepository } from '../adapters/repository/in-memory-user-repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 
