77703d76e2806ef6356c74bd39ffbf7a634eda23
Otavio Lemos <otaviolemos@gmail.com>
2020-07-19 19:56:46 -0300

    refactor: removed user from register use case constructor

--- src/usecase/register-user-on-mailing-list.spec.ts
+++ src/usecase/register-user-on-mailing-list.spec.ts
@@ -7,7 +7,7 @@ test('should register new user on mailing list', () => {
   const user: User = new User('Otávio Lemos', 'otaviolemos@gmail.com')
   const users: User[] = []
   const repo: UserRepository = new InMemoryUserRepository(users)
-  const sut = new RegisterUserOnMailingList(user, repo)
+  const sut = new RegisterUserOnMailingList(repo)
   expect(sut.registerUserOnMailingList(user)).toBeTruthy()
 })
 
@@ -15,6 +15,6 @@ test('should not register existing user on mailing list', () => {
   const user: User = new User('Otávio Lemos', 'otaviolemos@gmail.com')
   const users: User[] = [user]
   const repo: UserRepository = new InMemoryUserRepository(users)
-  const sut = new RegisterUserOnMailingList(user, repo)
+  const sut = new RegisterUserOnMailingList(repo)
   expect(sut.registerUserOnMailingList(user)).toBeFalsy()
 })
--- src/usecase/register-user-on-mailing-list.ts
+++ src/usecase/register-user-on-mailing-list.ts
@@ -2,7 +2,7 @@ import { User } from '../entities/user'
 import { UserRepository } from './port/user-repository'
 
 export class RegisterUserOnMailingList {
-  constructor (public user: User, public userRepository: UserRepository) {}
+  constructor (public userRepository: UserRepository) {}
 
   registerUserOnMailingList (user: User): boolean {
     return this.userRepository.add(user)
