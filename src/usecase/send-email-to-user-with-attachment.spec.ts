d9579a8ce880af6cf19373ec2cbca0c00189dd2c
Otavio Lemos <otaviolemos@gmail.com>
2020-07-26 12:28:20 -0300

    test: add test for mail service error

--- src/usecase/send-email-to-user-with-attachment.spec.ts
+++ src/usecase/send-email-to-user-with-attachment.spec.ts
@@ -2,34 +2,47 @@ import { SendEmailToUserWithAttachment } from './send-email-to-user-with-attachm
 import { MailService } from './port/mail-service'
 import * as fs from 'fs'
 
-const makeSut = (): SendEmailToUserWithAttachment => {
-  class MailServiceStub implements MailService {
-    async send (mailInfo: Object): Promise<boolean> {
-      return true
-    }
+// todo: generalize sendEmail use case: make it receive:
+// fromName, fromEmail, toName, toEmail, subject, emailBody, emailBodyHtml, attachments?
+
+class MailServiceStub implements MailService {
+  async send (mailInfo: Object): Promise<boolean> {
+    return true
   }
+}
+
+const makeSut = (): { sut: SendEmailToUserWithAttachment, mailServiceStub: MailServiceStub } => {
   const mailServiceStub = new MailServiceStub()
-  return new SendEmailToUserWithAttachment(mailServiceStub)
+  const sut = new SendEmailToUserWithAttachment(mailServiceStub)
+  return { sut, mailServiceStub }
+}
+
+const attachmentFilePath: string = 'test.txt'
+
+var mailInfo = {
+  from: 'Test <formEmail@gmail.com>',
+  to: 'Otávio Lemos <otaviolemos@gmail.com>',
+  subject: 'Attachment',
+  text: 'Hello world attachment test',
+  html: '<b>Hello world attachment test HTML</b>',
+  attachments: [{
+    filename: attachmentFilePath,
+    contentType: 'text/plain'
+  }]
 }
 
 test('should email user with attachment', async () => {
-  const attachmentFilePath: string = 'test.txt'
   const data = new Uint8Array(Buffer.from('testing 1 2 3'))
   createFile(attachmentFilePath, data)
-  var mailInfo = {
-    from: 'Test <formEmail@gmail.com>',
-    to: 'Otávio Lemos <otaviolemos@gmail.com>',
-    subject: 'Attachment',
-    text: 'Hello world attachment test',
-    html: '<b>Hello world attachment test HTML</b>',
-    attachments: [{
-      filename: attachmentFilePath,
-      contentType: 'text/plain'
-    }]
-  }
-  const sut = makeSut()
+  const { sut } = makeSut()
   expect(await sut.sendEmailToUserWithAttachment(mailInfo)).toBeTruthy()
   deleteFile(attachmentFilePath)
+})
+
+test('should raise error when email service fails', async () => {
+  const { sut, mailServiceStub } = makeSut()
+  jest.spyOn(mailServiceStub, 'send').mockReturnValueOnce(Promise.resolve(false))
+  expect(await sut.sendEmailToUserWithAttachment(mailInfo)).toBeFalsy()
 })
 
 function createFile (name: string, data: Uint8Array): void {
