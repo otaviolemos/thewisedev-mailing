269997f820205ffad3d6ead6c5e4d642bce70172
Otavio Lemos <otaviolemos@gmail.com>
2020-07-28 18:18:43 -0300

    refactor: move service to configuration file too

--- src/adapters/mail-service/nodemailler-mail-service.ts
+++ src/adapters/mail-service/nodemailler-mail-service.ts
@@ -2,13 +2,15 @@ import { MailService, MailOptions } from '../../usecase/port/mail-service'
 import config from '../../config/config'
 import * as nodemailer from 'nodemailer'
 
+// TODO: fix bug - attachment is not working
+
 export class NodemailerMailService implements MailService {
   async send (options: MailOptions): Promise<boolean> {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: config.get('email.service'),
       auth: {
-        user: config.get('gmailcredentials.username'),
-        pass: config.get('gmailcredentials.password')
+        user: config.get('email.username'),
+        pass: config.get('email.password')
       }
     })
 
