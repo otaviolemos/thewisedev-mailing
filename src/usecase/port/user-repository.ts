12995c83a1f4caec8890e021f0bd81ce0c2ed9d3
Otavio Lemos <otaviolemos@gmail.com>
2020-08-01 21:19:18 -0300

    feat: add UserData interface

--- src/usecase/port/user-repository.ts
+++ src/usecase/port/user-repository.ts
@@ -1,8 +1,13 @@
 import { User } from '../../domain/user'
 
+export interface UserData {
+  name: string
+  email: string
+}
+
 export interface UserRepository {
   findAllUsers: () => Promise<User[]>
   findUserByEmail: (email: string) => Promise<User>
-  add: (user: User) => Promise<void>
+  add: (user: UserData) => Promise<void>
   exists: (email: string) => Promise<boolean>
 }
