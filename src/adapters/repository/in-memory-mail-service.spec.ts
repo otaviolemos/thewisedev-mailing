e7c3f2c35222c8fd554e6ec75884475cf8481ec7
Otavio Lemos <otaviolemos@gmail.com>
2020-08-02 10:29:57 -0300

    test: add tests for additional in memory user repository

--- src/adapters/repository/in-memory-mail-service.spec.ts
+++ src/adapters/repository/in-memory-mail-service.spec.ts
@@ -3,7 +3,7 @@ import { InMemoryUserRepository } from './in-memory-user-repository'
 import { UserNotFoundError } from '../../usecase/port/errors/user-not-found-error'
 
 describe('In memory User repository', () => {
-  test('should throw errro is user is not found', async () => {
+  test('should throw error if user is not found', async () => {
     const users: User[] = []
     const userRepo = new InMemoryUserRepository(users)
     try {
@@ -11,5 +11,20 @@ describe('In memory User repository', () => {
     } catch (error) {
       expect(error).toBeInstanceOf(UserNotFoundError)
     }
+  })
+
+  test('should return user if user is found', async () => {
+    const users: User[] = [{ name: 'any_name', email: 'any_email@mail.com' }]
+    const userRepo = new InMemoryUserRepository(users)
+    const user = await userRepo.findUserByEmail('any_email@mail.com')
+    expect(user.email).toEqual('any_email@mail.com')
+  })
+
+  test('should add user', async () => {
+    const users: User[] = []
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name: 'any_name', email: 'any_email@mail.com' })
+    const user = await userRepo.findUserByEmail('any_email@mail.com')
+    expect(user.email).toEqual('any_email@mail.com')
   })
 })
