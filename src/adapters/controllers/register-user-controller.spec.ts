64c477f15d382bfe26d7347c0f1db47835d79c45
Otavio Lemos <otaviolemos@gmail.com>
2020-08-09 12:46:32 -0300

    test: ensure RegisterUserController returns an error if no name is provided

--- src/adapters/controllers/register-user-controller.spec.ts
+++ src/adapters/controllers/register-user-controller.spec.ts
@@ -10,5 +10,6 @@ describe('Register User Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name.'))
   })
 })
