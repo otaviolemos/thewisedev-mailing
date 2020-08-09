046e1ab800f3dbf5c56da5bdae6a75c90ff1dfd9
Otavio Lemos <otaviolemos@gmail.com>
2020-08-09 12:55:26 -0300

    feat: ensure RegisterUserController returns an error if no email is provided

--- src/adapters/controllers/register-user-controller.ts
+++ src/adapters/controllers/register-user-controller.ts
@@ -1,8 +1,17 @@
 export class RegisterUserController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name.')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name.')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email.')
+      }
     }
   }
 }
