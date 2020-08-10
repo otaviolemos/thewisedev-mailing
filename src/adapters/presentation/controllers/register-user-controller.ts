1f92e9f013650609b057a450dae527e887b63e5f
Otavio Lemos <otaviolemos@gmail.com>
2020-08-10 15:04:32 -0300

    feat: ensure register user constroller returns 400 if an invalid email is provided

--- src/adapters/presentation/controllers/register-user-controller.ts
+++ src/adapters/presentation/controllers/register-user-controller.ts
@@ -1,14 +1,24 @@
 import { HttpRequest, HttpResponse } from './ports/http'
 import { MissingParamError } from './errors/missing-param-error'
 import { badRequest } from './helpers/http-helper'
+import { EmailValidator } from './ports/email-validator'
+import { InvalidParamError } from './errors/invalid-param-error'
 
 export class RegisterUserController {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
