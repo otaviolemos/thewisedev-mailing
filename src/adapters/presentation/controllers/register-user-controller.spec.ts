ed0a3618283a0d67707adf623cfee2e388662880
Otavio Lemos <otaviolemos@gmail.com>
2020-08-10 17:26:19 -0300

    refactor: remove email validator with error factory and use mock implementation

--- src/adapters/presentation/controllers/register-user-controller.spec.ts
+++ src/adapters/presentation/controllers/register-user-controller.spec.ts
@@ -16,15 +16,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): SutType => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new RegisterUserController(emailValidatorStub)
@@ -84,8 +75,10 @@ describe('Register User Controller', () => {
   })
 
   test('should return 500 if email validator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new RegisterUserController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const httpRequest = {
       body: {
         name: 'any_name',
