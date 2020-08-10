19a38805cf78c3944dd4ded592f9a2115dc0f84b
Otavio Lemos <otaviolemos@gmail.com>
2020-08-10 15:04:49 -0300

    test: ensure register user constroller returns 400 if an invalid email is provided

--- src/adapters/presentation/controllers/register-user-controller.spec.ts
+++ src/adapters/presentation/controllers/register-user-controller.spec.ts
@@ -1,13 +1,27 @@
 import { RegisterUserController } from './register-user-controller'
 import { MissingParamError } from './errors/missing-param-error'
+import { InvalidParamError } from './errors/invalid-param-error'
+import { EmailValidator } from './ports/email-validator'
 
-const makeSut = (): RegisterUserController => {
-  return new RegisterUserController()
+interface SutType {
+  sut: RegisterUserController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutType => {
+  class EmailValidatorStub {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new RegisterUserController(emailValidatorStub)
+  return { sut, emailValidatorStub }
 }
 
 describe('Register User Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
@@ -19,7 +33,7 @@ describe('Register User Controller', () => {
   })
 
   test('should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name'
@@ -28,5 +42,19 @@ describe('Register User Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('should return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name@mail.com',
+        email: 'invalid_email'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
