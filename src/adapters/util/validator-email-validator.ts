c8beae4836f5ff411f59e540625b4e2da0885c12
Otavio Lemos <otaviolemos@gmail.com>
2020-08-16 12:20:41 -0300

    feat: email validator adapter

--- src/adapters/util/validator-email-validator.ts
+++ src/adapters/util/validator-email-validator.ts
@@ -1,7 +1,8 @@
 import { EmailValidator } from '../presentation/controllers/ports/email-validator'
+import validator from 'validator'
 
 export class ValidatorEmailValidator implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
