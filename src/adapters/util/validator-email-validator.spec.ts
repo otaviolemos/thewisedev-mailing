ca609b104b7cd86a24584ea9ca131f0911b7faf2
Otavio Lemos <otaviolemos@gmail.com>
2020-08-16 12:20:57 -0300

    test: email validator adapter

--- src/adapters/util/validator-email-validator.spec.ts
+++ src/adapters/util/validator-email-validator.spec.ts
@@ -1,9 +1,23 @@
 import { ValidatorEmailValidator } from './validator-email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('Email validator adapter', () => {
   test('should return false if validator returns false', () => {
     const sut = new ValidatorEmailValidator()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('should return true if validator returns true', () => {
+    const sut = new ValidatorEmailValidator()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
