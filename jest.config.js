0186550cf99a21b254149a59fc9780c5c61fb346
Otavio Lemos <otaviolemos@gmail.com>
2020-08-19 16:29:23 -0300

    chore: removed files that do not need to be covered by tests from test coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-ports.ts',
+    '!**/ports/**',
+    '!**/test/**',
+    '!**/config/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
