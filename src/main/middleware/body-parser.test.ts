d9c3cee6f4579594cf19f9c10d4d2a3ea61c779c
Otavio Lemos <otaviolemos@gmail.com>
2020-08-22 14:32:14 -0300

    feat: ensure requests parse body as json

--- src/main/middleware/body-parser.test.ts
+++ src/main/middleware/body-parser.test.ts
@@ -1,6 +1,14 @@
-describe('Body parser', () => {
-  test('test stub', () => {
-    const stub = true
-    expect(stub).toBe(true)
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Otavio' })
+      .expect({ name: 'Otavio' })
   })
 })
