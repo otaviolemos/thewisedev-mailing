6bbe51cff294e6089a969fc8d90023dec13ba812
Otavio Lemos <otaviolemos@gmail.com>
2020-08-23 12:10:47 -0300

    feat: ensure cors is enabled

---
+++ src/main/middleware/cors.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable CORS', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-headers', '*')
+      .expect('access-control-allow-methods', '*')
+  })
+})
