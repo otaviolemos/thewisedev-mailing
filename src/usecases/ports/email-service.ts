8d5d1db2db9abf9743c18c47cd94378d999797bd
Otavio Lemos <otaviolemos@gmail.com>
2020-10-09 14:57:31 -0300

    refactor: change mailOptions fields to readonly to avoid side-effects

--- src/usecases/ports/email-service.ts
+++ src/usecases/ports/email-service.ts
@@ -1,15 +1,15 @@
 
 export interface EmailOptions {
-  host: string
-  port: number
-  username: string
-  password: string
-  from: string
-  to: string
-  subject: string
-  text: string
-  html: string
-  attachments: Object[]
+  readonly host: string
+  readonly port: number
+  readonly username: string
+  readonly password: string
+  readonly from: string
+  readonly to: string
+  readonly subject: string
+  readonly text: string
+  readonly html: string
+  readonly attachments: Object[]
 }
 
 export interface EmailService {
