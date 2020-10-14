a1d20ce6f6b826475658dfca643367062259f944
Otavio Lemos <otaviolemos@gmail.com>
2020-10-14 14:39:22 -0300

    refactor: improve email validator function code

--- src/domain/validators.ts
+++ src/domain/validators.ts
@@ -1,19 +1,29 @@
 var tester = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
 
 export function validateEmail (email: string): boolean {
-  if (!email) return false
+  if (!email) {
+    return false
+  }
 
-  if (email.length > 256) return false
+  if (email.length > 256) {
+    return false
+  }
 
-  if (!tester.test(email)) return false
+  if (!tester.test(email)) {
+    return false
+  }
 
   var [account, address] = email.split('@')
-  if (account.length > 64) return false
+  if (account.length > 64) {
+    return false
+  }
 
   var domainParts = address.split('.')
   if (domainParts.some(function (part) {
     return part.length > 63
-  })) return false
+  })) {
+    return false
+  }
 
   return true
 }
