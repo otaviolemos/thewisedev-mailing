bd81c1c479124533b64715de4dac70718123dda9
Otavio Lemos <otaviolemos@gmail.com>
2020-10-15 15:21:14 -0300

    refactor: arranging error on the left and success on the right for result type

--- src/usecases/send-email-to-user-with-bonus/send-email-response.ts
+++ src/usecases/send-email-to-user-with-bonus/send-email-response.ts
@@ -1,4 +1,4 @@
 import { Either, Result } from '../../shared/result'
 import { MailServiceError } from '../ports/errors/mail-service-error'
 
-export type SendEmailResponse = Either<MailServiceError | Result<any>, Result<void>>
+export type SendEmailResponse = Either<MailServiceError, Result<any> | Result<void>>
