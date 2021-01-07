31316002a8d3a4e3f31c0e49edce8f46f79d8edb
Bruno Sousa <brse01@gmail.com>
2021-01-07 16:24:27 -0300

    refactor: standardization of error message formatting

--- src/adapters/presentation/controllers/errors/server-error.ts
+++ src/adapters/presentation/controllers/errors/server-error.ts
@@ -1,6 +1,6 @@
 export class ServerError extends Error implements ControllerError {
   constructor (reason: string) {
-    super('Server error: ' + reason + '.')
+    super(`Server error: ${reason}.`)
     this.name = 'ServerError'
   }
 }
