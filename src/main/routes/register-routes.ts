24233ff10e20b8636d68cdeefabe824b40006754
Otavio Lemos <otaviolemos@gmail.com>
2021-01-18 14:59:26 -0300

    refactor: disable eslint promise complaint

--- src/main/routes/register-routes.ts
+++ src/main/routes/register-routes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { makeRegisterUserController } from '../factories/register'
 import { adaptRoute } from '../adapters/express-route-adapter'
